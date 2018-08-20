0a62522a6b7471c7ae8898891d232e1d557eff42
Konrad Zdzieba <konrad.zdzieba@croscomsoftware.com>
2018-08-20 09:14:24 +0200

    devtool initial git

---
+++ lib/croscom_view_tool.rb
@@ -0,0 +1,6 @@
+require "croscom_view_tool/version"
+require "croscom_view_tool/renderer"
+
+module CroscomViewTool
+  # Your code goes here...
+end
---
+++ lib/croscom_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CroscomViewTool
+  VERSION = "0.1.0"
+end
